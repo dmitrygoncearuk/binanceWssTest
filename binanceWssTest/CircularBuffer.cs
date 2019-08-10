baf62eb4ed45ea5c55b5e5ec7a35fcf08fd12509
Dmitry Goncearuc <chaoticss@gmail.com>
2019-08-10 22:56:44 +0300

    time sync

--- binanceWssTest/CircularBuffer.cs
+++ binanceWssTest/CircularBuffer.cs
@@ -41,20 +41,4 @@ namespace binanceWssTest
             return count == 0 ? true : false;
         }
     }
-
-    [Serializable]
-    public struct EventStruct
-    {
-        public string e { get; set; } //eventType
-        public long E { get; set; } //eventTime
-        public string s { get; set; } //symbol
-        public int a { get; set; } //eventId
-        public string p { get; set; } //price
-        public string q { get; set; } //quantity
-        public int f { get; set; } //firstId
-        public int l { get; set; } //secondId
-        public long T { get; set; } //tradeTime
-        public bool m { get; set; } //marketMaker
-        public bool M { get; set; } //ignore
-    }
 }
--- binanceWssTest/Program.cs
+++ binanceWssTest/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Threading;
+using Newtonsoft.Json;
 
 namespace binanceWssTest
 {
     class Program
     {
+        public static double ConvertToUnixTimestamp(DateTime date)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan diff = date.ToUniversalTime() - origin;
+            return Math.Floor(diff.TotalMilliseconds);
+        }
+
         static void Main(string[] args)  //wss://stream.binance.com:9443/ws/ethbtc@aggTrade
         {
+            ServerTime serverTime;
+            DateTime now = DateTime.Now; 
+            using (var WebClient = new System.Net.WebClient())
+            {
+                var json = WebClient.DownloadString("https://api.binance.com/api/v1/time");
+                serverTime = JsonConvert.DeserializeObject<ServerTime>(json);
+            }
+            Console.WriteLine(serverTime.serverTime);
+            Console.WriteLine(ConvertToUnixTimestamp(now));
+            Console.ReadLine();
+
             Connection con1 = new Connection("ethbtc");
             Connection con2 = new Connection("ltcbtc");
             Thread t1 = new Thread(new ThreadStart(con1.Read));
